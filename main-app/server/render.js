a627854ee64f1ffff21352037b09029177285c76
Shariq Torres <shariq.torres@gmail.com>
2018-06-29 22:36:32 -0700

    added admin page

--- main-app/server/render.js
+++ main-app/server/render.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs");
 
-module.exports.render = function(templateName, cb){
+module.exports = function(templateName, cb){
     fs.readFile(__dirname + "/templates/" + templateName + ".html", "utf-8", (err, data) => {
         return cb(data);
     })
--- main-app/server/server.js
+++ main-app/server/server.js
@@ -4,14 +4,13 @@ const path = require('path');
 const templateRender = require("./render")
 
 app.use('/static', express.static(path.join(__dirname + '/../build/static')));
-//app.use('/', express.static(path.join(__dirname + '/../build')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../build/index.html'));
 });
 
 app.get('/admin', function(req, res){
-    templateRender.render("admin_login", (data)=>{
+    templateRender("admin_login", (data)=>{
         res.send(data)
     });
 })
