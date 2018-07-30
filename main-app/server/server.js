d0a8f7d362a51e12fc8ac43f796c9f71f9f90afb
Shariq Torres <shariq.torres@gmail.com>
2018-07-29 20:06:18 -0700

    started on dashboard page...

--- main-app/server/server.js
+++ main-app/server/server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const templateRender = require("./render")
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser')
+
+nunjucks.configure("server/templates", {
+    express:app,
+    autoescape:true
+});
 
 app.use('/static', express.static(path.join(__dirname + '/./templates/static')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     templateRender("index", (data) => {
@@ -12,10 +21,23 @@ app.get('/', function (req, res) {
 });
 
 app.get('/admin', function(req, res){
-    templateRender("admin_login", (data)=>{
-        res.send(data)
-    });
+    res.render("admin_login.html");
 })
+
+app.post("/admin", function(req, res){
+    const req_body = req.body;
+    if(req_body['username'] == "" && req_body['password'] == ""){
+        res.render("admin_login.html", {show_error: true});
+    }else if(req_body['username'] !== "admin" && req_body['password'] !== "admin"){
+        res.render("admin_login.html", {show_error: true});
+    }else{
+        res.redirect("/dashboard");
+    }
+});
+
+app.get("/dashboard", (function(req, res){
+    res.render("admin_dashboard.html");
+}));
 
 app.listen(3000);
 console.log("now listening on port 3000")
