d5df288af90921a682ffc24a75e80279ea51a51e
Shariq Torres <shariq.torres@gmail.com>
2018-09-11 22:27:30 -0700

    updated test data creation

--- main-app/server/createTestData.js
+++ main-app/server/createTestData.js
@@ -16,30 +16,29 @@ function makeText(){
     Etiam vehicula vitae velit eu imperdiet. Sed urna est, placerat eu bibendum vitae, commodo vitae dolor. Curabitur pharetra lacus sed arcu tristique luctus. Morbi quis nulla metus. Vestibulum facilisis vel lectus non commodo. Sed enim arcu, accumsan a dignissim eget, sollicitudin eget lacus. Mauris eget velit sed purus luctus luctus in nec mi. Duis egestas elementum suscipit. Donec erat tortor, vehicula nec pulvinar ac, semper vitae leo. Ut a dolor eu orci viverra interdum. Maecenas sodales malesuada magna, vel congue purus finibus vitae. Aenean non nisi odio. Vestibulum vel fermentum nulla.`
 }
 var coords = [
-    [-118.428758, 34.041645],
-    [-118.337409, 34.012165],
-    [-118.322878, 34.040587],
-    [-118.273536, 34.067862],
-    [-118.568758, 34.041645],
-    [-118.787409, 34.012165],
-    [-118.882878, 34.040587],
-    [-118.903536, 34.987862],
-    [-118.128758, 34.661645],
-    [-118.157409, 34.232165],
-    [-118.222878, 34.220587],
-    [-118.303536, 34.167862],
-    [-118.828758, 34.041645],
-    [-118.037409, 34.012165],
-    [-118.922878, 34.040587],
-    [-118.773536, 34.067862],
-    [-118.728758, 34.041645],
-    [-118.137409, 34.912165],
-    [-118.022878, 34.740587],
-    [-118.873536, 34.467862],
-    [-118.428758, 34.041645],
-    [-118.337409, 34.012165],
-    [-118.322878, 34.040587],
-    [-118.273536, 34.067862]
+    [34.005645, -118.344056],
+    [34.013026, -118.339345],
+    [34.02323, -118.346228],
+    [34.019055, -118.337386],
+    [34.006003, -118.399921],
+    [34.019347, -118.396535],
+    [34.031824, -118.252667],
+    [34.042176, -118.241986],
+    [34.076470, -118.259651],
+    [34.076652, -118.268282],
+    [34.087829, -118.329119],
+    [34.097344, -118.333933],
+    [33.955462, -118.358698],
+    [33.946815, -118.357975],
+    [34.056852, -118.299590],
+    [34.054474, -118.307535],
+    [34.054889, -118.364435],
+    [34.056711, -118.364832],
+    [34.056711, -118.364832],
+    [34.058172, -118.336448],
+    [34.022507, -118.490133],
+    [34.086967, -118.358780],
+    [34.093595, -118.363195]    
 ]
 
 var decade  = [
@@ -48,7 +47,7 @@ var decade  = [
 
 function makeStoryRecords(){
     var storyParms = {
-        "title":"That time in Westside Pavilion",
+        "title":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore ",
         "location": [],
         "decade":0,
         "area":0,
@@ -76,16 +75,66 @@ function makeStoryRecords(){
     };
     var async_funcs = [];
     
-    for(var i = 0; i < 24; i++){
-        var randomLocation = Math.floor((Math.random() * 24) + 1); 
-        var randomNeighborhood = Math.floor((Math.random() * 12) + 1);
+    for(var i = 0; i < 23; i++){
         var sp = storyParms;
-        var randomYearSuffix = Math.floor((Math.random() * 14) + 1);
+        var randomYearSuffix = Math.floor((Math.random() * 9) + 1);
         var randomYearPrefix = Math.floor((Math.random() * 4) + 1);
         sp.context.year = decade[ randomYearPrefix ] + randomYearSuffix;
         sp.decade = decade[randomYearPrefix];
-        sp.location = coords[ randomLocation ];
-        sp.area = randomNeighborhood;
+        console.log(coords[i])
+        console.log(i)
+        sp.location = [coords[i][1], coords[i][0] ];
+        switch(i){
+            case 0:
+            case 1:
+                sp.area = constants.neighborhoods[0].value;
+            break;
+            case 2:
+            case 3:
+                sp.area = constants.neighborhoods[1].value;
+            break;
+            case 4:
+            case 5:
+                sp.area = constants.neighborhoods[2].value;
+            break;
+            case 6:
+            case 7:
+                sp.area = constants.neighborhoods[3].value;
+            break;
+            case 8:
+            case 9:
+                sp.area = constants.neighborhoods[4].value;
+            break;
+            case 10:
+            case 11:
+                sp.area = constants.neighborhoods[5].value;
+            break;
+            case 12:
+            case 13:
+                sp.area = constants.neighborhoods[6].value;
+            break;
+            case 14:
+            case 15:
+                sp.area = constants.neighborhoods[7].value;
+            break;
+            case 16:
+            case 17:
+                sp.area = constants.neighborhoods[8].value;
+            break;
+            case 18:
+            case 19:
+                sp.area = constants.neighborhoods[9].value;
+            break;
+            case 20:
+            case 21:
+                sp.area = constants.neighborhoods[10].value;
+            break;
+            case 22:
+            case 23:
+                sp.area = constants.neighborhoods[11].value;
+            break;
+
+        }
         async_funcs.push(StoryModel.model.createFromObj(sp));
         
     }
